a36fe8658c47703e42d0a60babcfbe59a96bf832
Anto Luque <105891198+antonelladluque@users.noreply.github.com>
2026-03-27 19:01:19 -0300

    Add files via upload

---
+++ ejercicio_tabla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	//ingresar la tabla del uno al nueve
+	
+	int i, num,resul; 
+	
+	printf("Que tabla desea saber, del 1 al 9?\n");
+	scanf("%i", &num);
+	
+	for(i=1; i<=10 ; i ++){
+		resul=i*num;
+		printf("%i x %i = %i\n", i , num, resul);
+	}
+	
+	return 0;
+}
+
