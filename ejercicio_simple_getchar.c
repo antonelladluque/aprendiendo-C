16e2124acef39ceb51578823a0af561fe872084c
Anto Luque <105891198+antonelladluque@users.noreply.github.com>
2026-04-27 17:39:38 -0300

    Create ejercicio_simple_getchar.c

---
+++ ejercicio_simple_getchar.c
@@ -0,0 +1,30 @@
+/*Escribe un programa que lea caracteres uno por uno desde el teclado. Si el carácter es una letra minúscula, debe mostrarla en mayúscula. Si es cualquier otro carácter (número, símbolo o espacio), lo debe mostrar tal cual. El programa debe detenerse cuando el usuario presione la tecla Enter.*/
+
+#include <stdio.h>
+
+int main() {
+    char c;
+
+    printf("Escribe un mensaje en minusculas (presiona Enter para terminar):\n");
+
+    // Leemos el primer caracter
+    c = getchar();
+
+    // Mientras el caracter no sea un salto de linea (Enter)
+    while (c != '\n') {
+        
+        // Verificamos si es una letra minuscula (entre 'a' y 'z')
+        if (c >= 'a' && c <= 'z') {
+            c = c - 32; // Convertimos a mayuscula
+        }
+
+        // Imprimimos el caracter resultante
+        putchar(c);
+
+        // Leemos el siguiente caracter para la proxima vuelta
+        c = getchar();
+    }
+
+    printf("\n\nPrograma finalizado.\n");
+    return 0;
+}
