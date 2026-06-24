027ffdd6457a4bdea37821a0a8bcb23b562213c6
Anto Luque <105891198+antonelladluque@users.noreply.github.com>
2026-06-24 12:24:17 -0300

    Update pilas_ejercicio7.2.2.c

--- tercer-parcial/pilas_ejercicio7.2.2.c
+++ tercer-parcial/pilas_ejercicio7.2.2.c
@@ -1,43 +1,4 @@
-typedef struct NodoPila {
-    int dato;
-    struct NodoPila* sgte;
-} NodoPila;
-
-void push(NodoPila** tope, int valor) {
-    NodoPila* nuevo = (NodoPila*)malloc(sizeof(NodoPila));
-    nuevo->dato = valor;
-    nuevo->sgte = *tope;
-    *tope = nuevo;
-}
-
-int pop(NodoPila** tope) {
-    if (*tope == NULL) return -1;
-    NodoPila* aux = *tope;
-    int valor = aux->dato;
-    *tope = aux->sgte;
-    free(aux);
-    return valor;
-}
-
-void Reemplazar(NodoPila** tope, int nuevo, int viejo) { // 
-    NodoPila* pilaAux = NULL;
-    
-    // Desapilar y reemplazar
-    while (*tope != NULL) {
-        int valor = pop(tope);
-        if (valor == viejo) {
-            push(&pilaAux, nuevo);
-        } else {
-            push(&pilaAux, valor);
-        }
-    }
-    
-    // Restaurar la pila original
-    while (pilaAux != NULL) {
-        push(tope, pop(&pilaAux));
-    }
-}
-Ejercicio 7.2.3: Verificar si una Pila está ordenada ascendentementeJustificación de Complejidad: El orden del algoritmo es $O(n)$ porque obligatoriamente debemos vaciar la pila entera para revisar cada elemento y luego volver a llenarla para dejarla en su estado original .Cint estaOrdenadaAscendente(NodoPila** tope) { // 
+int estaOrdenadaAscendente(NodoPila** tope) { // 
     if (*tope == NULL || (*tope)->sgte == NULL) return 1; // Vacía o con 1 elemento está ordenada
     
     NodoPila* pilaAux = NULL;
